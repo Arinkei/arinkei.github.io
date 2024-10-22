909c481f05862d9b89fc6a707a4d6b8f7f8fce84
Arinkei <106501116+Arinkei@users.noreply.github.com>
2024-10-22 18:53:23 +0900

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,8 +1,19 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 60) { // 스크롤 위치가 60px 이상이면 스타일 변경
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
+// DOMContentLoaded 이벤트 처리 후 navbar.html을 불러오기
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-root').innerHTML = data;
+
+            // 스크롤 이벤트 추가
+            const navbar = document.querySelector('.navbar');
+            window.addEventListener('scroll', function() {
+                if (window.scrollY > 60) { // 스크롤 위치가 60px 이상이면 스타일 변경
+                    navbar.classList.add('scrolled');
+                } else {
+                    navbar.classList.remove('scrolled');
+                }
+            });
+        })
+        .catch(error => console.error('Error loading the navbar:', error));
 });
