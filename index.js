411e488cb79e35fbf7e88b66cfa004ca1c15e4b8
Arinkei <106501116+Arinkei@users.noreply.github.com>
2024-10-24 15:38:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.addEventListener('scroll', function() {
         const lineRect = line.getBoundingClientRect();
 
         // 요소가 화면 중앙 근처에 있을 때 페이드 인/아웃 적용
-        if (lineRect.top >= windowHeight * 0.3 && lineRect.top - window.scrollY > 10) {
+        if (lineRect.top >= windowHeight * 0.3 && lineRect.top - window.scrollY > 100) {
             line.style.opacity = 1;
         } else {
             line.style.opacity = 0;
