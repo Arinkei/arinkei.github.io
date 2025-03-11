f25f098e339f76c0828c691aff95384355cd0de7
Arinkei <arinkei@naver.com>
2025-03-11 16:24:04 +0900

    md

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,10 @@
 export default function Footer() {
     return (
       <footer style={{ backgroundColor: '#f1f1f1', padding: '20px', textAlign: 'center' }}>
-        <p>© 2025 My Website. All rights reserved.</p>
+        <div>
+            <a className="">my home</a>
+        </div>
+        <p>© 2025 PGO. All rights reserved.</p>
       </footer>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// Footer.js
+export default function Header() {
+    return (
+      <header style={{ backgroundColor: '#ffffd0', padding: '20px', textAlign: 'center' }}>
+        <div className="hover">
+            <ref>my home</ref>
+            <a>your's</a>
+            <a>here</a>
+        </div>
+      </header>
+    );
+  }
+   
