bca189e465e22648ef3767c5435cdcdff5c88840
Arinkei <arinkei@naver.com>
2025-03-11 16:01:46 +0900

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import Footer from '../components/Footer';
+
 export default function Home() {
   return (
     <div>
       <main>
         <a>a</a>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+// Footer.js
+export default function Footer() {
+    return (
+      <footer style={{ backgroundColor: '#f1f1f1', padding: '20px', textAlign: 'center' }}>
+        <p>© 2025 My Website. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
