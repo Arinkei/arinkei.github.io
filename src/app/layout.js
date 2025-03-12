4de184ad43235a686b31d7ad4a4eea7e25d363b6
Arinkei <arinkei@naver.com>
2025-03-12 10:26:32 +0900

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "PGO",
   description: "PlayGround Only",
+  icons:{
+    icon:"/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
